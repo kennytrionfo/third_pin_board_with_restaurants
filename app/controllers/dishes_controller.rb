5a9e3fb085d3be8b4ccf565fca1764bb8c2b855c
kennytrionfo <kentrionfo@gmail.com>
2015-04-08 19:31:15 -0600

    added stuff to dishes controller from other app to compare

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -49,4 +49,23 @@ class DishesController < ApplicationController
 		@dish = Dish.find(params[:id])
 	end
 
+#————from fit right————Kenny Trionfo Apr 8,2015————
+	before_action :set_todo_list
+
+	def create
+		@todo_item = @todo_list.todo_items.create(todo_item_params)
+		redirect_to @todo_list 
+	end
+
+
+	private
+
+	def set_todo_list 
+		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def todo_item_params
+		params[:todo_item].permit(:content)
+	end
+
 end
